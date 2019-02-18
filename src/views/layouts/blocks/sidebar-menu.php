5da7505df4580c1d893274f58d31b34065810e43
Jomon Johnson <jomon.entero@gmail.com>
2019-02-18 21:58:45 +0530

    init

--- src/views/layouts/blocks/sidebar-menu.php
+++ src/views/layouts/blocks/sidebar-menu.php
@@ -17,18 +17,18 @@ use yii\helpers\Html;
 
 //run set-sidebar-toggle action while clicking sidebar toggle button
 
-if (UserSettings::get('sidebar.closed') == true) {
-    Html::addCssClass($this->params['body']['options'], 'page-sidebar-closed');
-} else {
-    Html::removeCssClass($this->params['body']['options'], 'page-sidebar-closed');
-}
-$url = Url::to(['/user-settings/set-sidebar-toggle']);
-$script = <<< JS
-$(document).on('click','.page-header-inner .menu-toggler',function(){
-     $.ajax({  url: '{$url}',  data: {status: $('body').hasClass('page-sidebar-closed')},});
-});
-JS;
-$this->registerJs($script, View::POS_END);
+//if (UserSettings::get('sidebar.closed') == true) {
+//    Html::addCssClass($this->params['body']['options'], 'page-sidebar-closed');
+//} else {
+Html::removeCssClass($this->params['body']['options'], 'page-sidebar-closed');
+//}
+//$url = Url::to(['/user-settings/set-sidebar-toggle']);
+//$script = <<< JS
+//$(document).on('click','.page-header-inner .menu-toggler',function(){
+//     $.ajax({  url: '{$url}',  data: {status: $('body').hasClass('page-sidebar-closed')},});
+//});
+//JS;
+//$this->registerJs($script, View::POS_END);
 
 $config = \codexten\yii\metronic4\Theme::getComponent()->sidebarMenuConfig;
 ?>
